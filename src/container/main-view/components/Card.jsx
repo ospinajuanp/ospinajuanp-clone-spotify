a69a03a81ed5007e848241c76fd7fdc1e35be9ff
Ospinajuanp <juanpo996@gmail.com>
2024-03-24 14:25:50 -0500

    Add btn play on card

--- src/container/main-view/components/Card.jsx
+++ src/container/main-view/components/Card.jsx
@@ -1,3 +1,4 @@
+import { FaPlay } from "react-icons/fa";
 import './Card.css'
 
 function Card() {
@@ -6,6 +7,9 @@ return (
     <div className='Card'>
         <div className='Card__image'>
             <img src="https://i.pinimg.com/736x/00/8a/a4/008aa48c412fecfffe26b8fb8ec662b4.jpg" alt="blessd"  />
+            <div className="Card__play">
+                <FaPlay />
+            </div>
         </div>
         <div className='Card__text'>
             <div className='Card__text-title'>
